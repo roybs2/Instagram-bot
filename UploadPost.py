12e0fafe13c2437a8bf709be5c090d971e9c62d2
Roy <roybe@payoneer.com>
2018-08-22 19:42:28 +0300

    add auto upload

--- UploadPost.py
+++ UploadPost.py
@@ -2,6 +2,12 @@ from InstagramAPI import InstagramAPI
 import ConfigParser
 import LogHandler
 import imp
+import os
+import base64
+import time
+from google_images_download import google_images_download
+from random import randint
+import schedule
 singletonClass = imp.load_source('Singleton', 'Utilities/Singleton.py')
 
 @singletonClass.singleton
@@ -17,6 +23,19 @@ class Upload:
         self.InstagramAPI = InstagramAPI(config.get('DEFAULT','userName'), config.get('DEFAULT','password'))
         self.InstagramAPI.login()  # login
 
+
+
+
+        #Download pictures:
+        self.searchKeyword = "best success quotes"
+        argumnets = {
+            "keywords": self.searchKeyword,
+            "limit": 50,
+            "print_urls": True
+        }
+        response = google_images_download.googleimagesdownload()
+        self.images = response.download(argumnets)
+
     def UploadPost(self, postPath, title):
         try:
             tags = '#' + self.captionTags
@@ -28,26 +47,51 @@ class Upload:
                 "Unable to upload post! post path: {}, caption: {}. Exception: {}".format(postPath, caption, e))
             raise Exception('Unable to upload post!')
 
+    def CheckIfImageExistInFolder(self, image):
+        s = image.rfind('\\')
+        folderPath = image[:s]
+        print folderPath
+        print image
+        for file in os.listdir(folderPath):
+            if base64.b64encode(file) == base64.b64encode(image):
+                return False
+        return True
 
+    def AutoUploadImage(self):
+        randomNumber = randint(0,50)
 
+        # todo: add check if file is image
+        if self.CheckIfImageExistInFolder(self.images[self.searchKeyword][randomNumber]):
+            self.UploadPost(self.images[self.searchKeyword][randomNumber], 'So, how was your day? :)')
+
+    def ScheduleUpload(self):
+        schedule.every(10).minutes.do(self.AutoUploadImage)
+
+        while 1:
+            schedule.run_pending()
 
 #photo_path = r'C:\Roy-Personal\temp\14_14.jpg'
 
 
-from google_images_download import google_images_download
+a= Upload()
+a.ScheduleUpload()
 
-searchKeyword = "best success quotes"
-argumnets = {
-        "keywords": searchKeyword,
-        "limit": 2,
-        "print_urls": True
-    }
 
-response = google_images_download.googleimagesdownload()
-images = response.download(argumnets)
-print images
-print images[searchKeyword]
-a = Upload()
-for imagePath in images[searchKeyword]:
-    a.UploadPost(imagePath, 'So, how was your day? :)')
-    print imagePath
+# searchKeyword = "best success quotes"
+# argumnets = {
+#         "keywords": searchKeyword,
+#         "limit": 3,
+#         "print_urls": True
+#     }
+#
+# response = google_images_download.googleimagesdownload()
+# images = response.download(argumnets)
+# a = Upload()
+# for imagePath in images[searchKeyword]:
+#     if a.CheckIfImageExistInFolder(imagePath):
+#         a.UploadPost(imagePath, 'So, how was your day? :)')
+#         time.sleep(15)
+#
+
+
+
