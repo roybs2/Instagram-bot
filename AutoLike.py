121c9d1cea9745c9cc41ce74fe851bd7ad5b26ed
Roy <abab.ben@gmail.com>
2018-08-25 22:41:49 +0300

    add class auto liker

--- AutoLike.py
+++ AutoLike.py
@@ -4,126 +4,127 @@ import LogHandler
 import SeleniumConfig
 import selenium
 
-global driver, userName, password, config, likeClassName, unLikeClassName, logger, numberOfLikes
+class AutoLike:
+    def __init__(self):
 
-def Init():
-    global driver, config, userName, password, likeClassName, unLikeClassName, logger, numberOfLikes
+        self.logger = LogHandler.Start()
+        self.logger.getLogger('AutoLike')
 
-    logger = LogHandler.Start()
-    logger.getLogger('AutoLike')
-
-    try:
-        driver = SeleniumConfig.InitSelenium()
-
-        config = ConfigParser.ConfigParser()
-        config.read('config.ini')
-        userName = config.get('DEFAULT','userName')
-        password = config.get('DEFAULT','password')
-        likeClassName = config.get('DEFAULT', 'likeClassName')
-        unLikeClassName = config.get('DEFAULT', 'unLikeClassName')
-        numberOfLikes = config.get('DEFAULT', 'numberOfLikes')
-
-    except Exception as e:
-        logger.error("Unable to init chrome driver or config file. Exception: %s", e)
-        raise Exception('Unable to init')
-
-
-def Login():
-    try:
-        driver.get(config.get('DEFAULT','instagramLogin'))
-        time.sleep(2)
-
-        userNameTextBox = driver.find_element_by_name('username')
-        userNameTextBox.send_keys(userName)
-        passwordTextBox = driver.find_element_by_name('password')
-        passwordTextBox.send_keys(password)
-
-        loginButton = driver.find_element_by_class_name('_5f5mN')
-        loginButton.click()
-    except Exception as e:
-        logger.error("Unable to login to instagram with username:{}, psasword:{}. Exception: {}".format(userName, password, e))
-        raise Exception('Internal Error')
-
-
-# select the first option of the search
-def Search(Tag):
-    searchTextBox = driver.find_element_by_class_name('XTCLo')
-    searchTextBox.send_keys(Tag)
-    time.sleep(1)
-    searchResult = driver.find_elements_by_class_name('yCE8d')
-    searchResult[0].click()
-
-
-
-def AutoLike():
-    try:
-        likeButton = driver.find_element_by_class_name(likeClassName)
-        likeButton.click()
-    except Exception as e:
         try:
-            driver.find_element_by_class_name(unLikeClassName)
-            logger.info("Post was already liked")
-        except Exception as ex:
-            logger.error("Unable to do like with class name: {}, url: {}. Exception: {}".format(likeClassName,
-                                                                                                driver.current_url, e))
+            self.driver = SeleniumConfig.InitSelenium()
 
-
-def AutoLiker(numberOfLiks):
-    tags = config.get('DEFAULT', 'tags')
-    tagPagePrefix = config.get('DEFAULT', 'instagramTagPagePrefix')
-    postClassName = config.get('DEFAULT', 'postClassName')
-
-    listOfTags = tags.split(",")
-    for tag in listOfTags:
-        try:
-            driver.get("{}{}".format(tagPagePrefix, tag))
-
-            listOfPosts = driver.find_elements_by_class_name(postClassName)
-            if(numberOfLikes > len(listOfPosts)):
-                maxRange = len(listOfPosts)
-            else:
-                maxRange = numberOfLikes
-
-            for i in range(0, maxRange):
-                try:
-                    listOfPosts[i].click()
-                    time.sleep(1)
-                    AutoLike()
-
-                    driver.execute_script("window.history.go(-1)")
-                    time.sleep(1)
-                except Exception as e:
-                    logger.error("Can not do like for: tag: {}, postClassName: {}. Exception: {}".format(tag, postClassName, e))
-                    #raise Exception('Internal Error')
+            self.config = ConfigParser.ConfigParser()
+            self.config.read('config.ini')
+            self.userName = self.config.get('DEFAULT','userName')
+            self.password = self.config.get('DEFAULT','password')
+            self.likeClassName = self.config.get('DEFAULT', 'likeClassName')
+            self.unLikeClassName = self.config.get('DEFAULT', 'unLikeClassName')
+            self.numberOfLikes = self.config.get('DEFAULT', 'numberOfLikes')
 
         except Exception as e:
-            logger.error("Something in the auto liker went wrong. Exception: {}".format(e))
+            self.logger.error("Unable to init chrome driver or config file. Exception: %s", e)
+            self.driver.close()
+            raise Exception('Unable to init')
+
+    def Login(self):
+        try:
+            self.driver.get(self.config.get('DEFAULT','instagramLogin'))
+            time.sleep(2)
+
+            userNameTextBox = self.driver.find_element_by_name('username')
+            userNameTextBox.send_keys(self.userName)
+            passwordTextBox = self.driver.find_element_by_name('password')
+            passwordTextBox.send_keys(self.password)
+
+            loginButton = self.driver.find_element_by_class_name('_5f5mN')
+            loginButton.click()
+        except Exception as e:
+            self.logger.error("Unable to login to instagram with username:{}, psasword:{}. Exception: {}".format(self.userName, self.password, e))
+            print "Unable to login to instagram with username:{}, psasword:{}. Exception: {}".format(self.userName, self.password, e)
+            self.driver.close()
             raise Exception('Internal Error')
 
 
-
-def UploadPost(picture, textBox, tags):
-    upload = driver.find_element_by_class_name('coreSpriteFeedCreation')
-    upload.send_keys(picture)
-
-    nextButton = driver.find_element_by_class_name('_9glb8')
-    nextButton.click()
-
-    text = driver.find_element_by_tag_name('textarea')
-    text.send_keys(textBox)
-    text.send_keys(selenium.webdriver.Keys.RETURN);
-    text.send_keys(tags)
+    # select the first option of the search
+    def Search(self, Tag):
+        searchTextBox = self.driver.find_element_by_class_name('XTCLo')
+        searchTextBox.send_keys(Tag)
+        time.sleep(1)
+        searchResult = self.driver.find_elements_by_class_name('yCE8d')
+        searchResult[0].click()
 
 
-def MainAutoLiker():
-    Init()
-    Login()
 
-    time.sleep(1)
-    AutoLiker(int(numberOfLikes))
+    def AutoLikerForPost(self):
+        try:
+            likeButton = self.driver.find_element_by_class_name(self.likeClassName)
+            likeButton.click()
+        except Exception as e:
+            try:
+                self.driver.find_element_by_class_name(self.unLikeClassName)
+                self.logger.info("Post was already liked")
+            except Exception as ex:
+                self.logger.error("Unable to do like with class name: {}, url: {}. Exception: {}".format(self.likeClassName,
+                                                                                                         self.driver.current_url, e))
 
-    driver.close()
+
+    def AutoLiker(self, numberOfLiks):
+        tags = self.config.get('DEFAULT', 'tags')
+        tagPagePrefix = self.config.get('DEFAULT', 'instagramTagPagePrefix')
+        postClassName = self.config.get('DEFAULT', 'postClassName')
+
+        listOfTags = tags.split(",")
+        for tag in listOfTags:
+            try:
+                self.driver.get("{}{}".format(tagPagePrefix, tag))
+
+                listOfPosts = self.driver.find_elements_by_class_name(postClassName)
+                if(self.numberOfLikes > len(listOfPosts)):
+                    maxRange = len(listOfPosts)
+                else:
+                    maxRange = self.numberOfLikes
+
+                for i in range(0, maxRange):
+                    try:
+                        listOfPosts[i].click()
+                        time.sleep(1)
+                        self.AutoLikerForPost()
+
+                        self.driver.execute_script("window.history.go(-1)")
+                        time.sleep(1)
+                    except Exception as e:
+                        self.logger.error("Can not do like for: tag: {}, postClassName: {}. Exception: {}".format(tag, postClassName, e))
+                        #raise Exception('Internal Error')
+
+            except Exception as e:
+                self.logger.error("Something in the auto liker went wrong. Exception: {}".format(e))
+                self.driver.close()
+                raise Exception('Internal Error')
 
 
+
+    def UploadPost(self, picture, textBox, tags):
+        upload = self.driver.find_element_by_class_name('coreSpriteFeedCreation')
+        upload.send_keys(picture)
+
+        nextButton = self.driver.find_element_by_class_name('_9glb8')
+        nextButton.click()
+
+        text = self.driver.find_element_by_tag_name('textarea')
+        text.send_keys(textBox)
+        text.send_keys(selenium.webdriver.Keys.RETURN);
+        text.send_keys(tags)
+
+
+    def MainAutoLiker(self):
+        self.Login()
+        time.sleep(1)
+        self.AutoLiker(int(self.numberOfLikes))
+
+        self.driver.close()
+
+
+#a = AutoLike()
+#a.MainAutoLiker()
 # to run without flask:
-MainAutoLiker()
+#MainAutoLiker()
--- Controller.py
+++ Controller.py
@@ -4,9 +4,11 @@ import AutoLike
 app = Flask(__name__)
 
 @app.route('/AutoLike', methods=['GET'])
-def AutoLikeController():
-    AutoLike.MainAutoLiker()
-    return '', 204
+def AutoLikeRequest():
+    print "Auto like controller started"
+    autoLikePost = AutoLike.AutoLike()
+    autoLikePost.MainAutoLiker()
+    return "Auto like controller finished", 200
 
 if __name__ == '__main__':
     app.run(debug=True)
